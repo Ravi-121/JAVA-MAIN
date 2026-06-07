e4b317a3884975d0c3c1a1b80580f1b2e9170da5
Ravi-121 <75595144+Ravi-121@users.noreply.github.com.>
2026-06-07 22:41:36 +0530

    Recursions

---
+++ Resursions.java
@@ -0,0 +1,17 @@
+public class Resursions {
+
+  public static int fact(int i) {
+
+    if (i == 0) {
+      return 1;
+    }
+    return i * fact(i - 1);
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(fact(0));
+
+  }
+
+}
